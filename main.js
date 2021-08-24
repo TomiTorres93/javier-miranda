a57d2309cf0abf0b13317f834decff0b5f87b8c9
tomi torres <tomitorres.93@gmail.com>
2021-08-24 16:47:05 -0300

    cambios finales

--- main.js
+++
@@ -1,4 +0,0 @@
-$('img').on('click', function (){
-    $('body').append('<div id="dialog" title="Image"><img src="' + $(this).attr('src') + '" width="300" /></div>');
-    $('#dialog').dialog();
-});
